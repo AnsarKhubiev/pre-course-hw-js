af11daca81827692559d7980e73587a1c314f3d8
AnsarKhubiev <anskhubiev@icloud.com>
2023-09-28 17:26:54 +0300

    home-works 3-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+const createColorString = (arr) => {
+    const newArr = arr.filter((e) => {
+        return e === "черный" || e === "красный" || e === "желтый";
+    });
+    return newArr.join("-");
+};
+
+console.log(createColorString(colors));
